6884f7fb2adeba4f2e19d8e8fe474e84f31e9614
sergio <sergiolopezantequera@gmail.com>
2025-03-03 09:27:23 +0100

    Update Tailwind.config.js nueva actualización

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
       colors: {
         'aqua-blue': '#0066cc', // Color personalizado
       },
-    },
+      animation: {
+        'float': 'float 6s ease-in-out infinite',
+        'fade-in': 'fadeIn 1s ease-out'
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' }
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
+        }
+      }
+    }
   },
   plugins: [],
 };
