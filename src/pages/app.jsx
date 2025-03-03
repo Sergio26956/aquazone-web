66e1c772b7d9d40724e96260e5d3f0da6f89b0af
sergio <sergiolopezantequera@gmail.com>
2025-03-03 08:07:42 +0100

    Update app.jsx

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,12 +1,13 @@
-import WhatsAppButton from '../../components/WhatsAppButton'; // Ruta corregida ✅
+// src/pages/_app.jsx
+   import WhatsAppButton from '@/components/WhatsAppButton'; // Ruta con alias ✅
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <WhatsAppButton /> {/* Botón añadido aquí */}
-    </>
-  );
-}
+   function MyApp({ Component, pageProps }) {
+     return (
+       <div className="min-h-screen bg-slate-50"> {/* Fondo general */}
+         <Component {...pageProps} />
+         <WhatsAppButton /> {/* Botón visible en TODAS las páginas */}
+       </div>
+     );
+   }
 
-export default MyApp;
+   export default MyApp;
