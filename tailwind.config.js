49eb47455c6f8dfdb70309def1b2b9c10a97d111
sergio <sergiolopezantequera@gmail.com>
2025-03-05 12:45:21 +0100

    Update Tailwind.config.js corregir errores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'blue-600': '#2563eb',
-        'blue-900': '#1e3a8a',
-      },
-    },
-  },
-  plugins: [],
-};
+     content: ['./src/**/*.{js,jsx,ts,tsx}'],
+     theme: { extend: {} },
+     plugins: [],
+   };
