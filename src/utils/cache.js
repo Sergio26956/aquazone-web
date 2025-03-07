340bda30556c23d71a1b8fac66dbd372d520d133
sergio <sergiolopezantequera@gmail.com>
2025-03-07 13:12:34 +0100

    Create cache.js

---
+++ src/utils/cache.js
@@ -0,0 +1,11 @@
+const cache = new Map();
+
+export function setCache(key, data, ttl = 3600) {
+  cache.set(key, { data, expires: Date.now() + ttl * 1000 });
+}
+
+export function getCache(key) {
+  const item = cache.get(key);
+  if (!item || Date.now() > item.expires) return null;
+  return item.data;
+}
