53ede2c2a262e8daa606d835601eba5b91e2a138
sergio <sergiolopezantequera@gmail.com>
2025-03-07 11:06:10 +0100

    Update index.jsx última actualizacion final

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,17 @@
-// src/pages/index.jsx
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import HeroSection from '@/components/HeroSection';
+import Testimonios from '@/components/Testimonios';
+import ScrollAnimation from '@/components/ScrollAnimation';
 
 export default function Home() {
   return (
     <div className="relative">
       <Navbar />
       <HeroSection />
+      <ScrollAnimation>
+        <Testimonios />
+      </ScrollAnimation>
       <Footer />
     </div>
   );
