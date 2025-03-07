0d72b061f13a865f463ab01f2892215f38f1e6f5
sergio <sergiolopezantequera@gmail.com>
2025-03-07 11:08:42 +0100

    Update WaterSound.jsx actualizacion final

--- src/components/WaterSound.jsx
+++ src/components/WaterSound.jsx
@@ -2,10 +2,19 @@ import { useEffect } from 'react';
 
 export default function WaterSound() {
   useEffect(() => {
-    const audio = new Audio('/audio/water-sound.mp3');
-    audio.loop = true;
-    audio.play();
-    return () => audio.pause();
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.type = 'sine';
+    oscillator.frequency.setValueAtTime(200, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(0.02, audioContext.currentTime);
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    oscillator.start();
+
+    return () => oscillator.stop();
   }, []);
 
   return null;
