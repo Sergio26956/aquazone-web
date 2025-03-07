04340fce31c4195b74fc3f3f274c3036ed82062d
sergio <sergiolopezantequera@gmail.com>
2025-03-07 15:26:30 +0100

    Update playas.jsx

--- src/pages/parques-flotantes/playas.jsx
+++ src/pages/parques-flotantes/playas.jsx
@@ -2,6 +2,8 @@ import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import Image from 'next/image';
 import SEO from '../../components/SEO';
+import Gallery from '../../components/Gallery';
+import Testimonios from '../../components/Testimonios';
 
 export default function Playas() {
   return (
@@ -15,8 +17,10 @@ export default function Playas() {
         <div className="max-w-7xl mx-auto px-4 text-center">
           <h1 className="text-4xl font-bold text-blue-900 mb-8">Montaje en Playas 🏖️</h1>
           <div className="relative h-96 w-full mb-12 rounded-xl overflow-hidden shadow-2xl">
-            <Image src="/images/Captura de pantalla_20220609_185212_com.huawei.himovie.overseas.webp" alt="Playas" layout="fill" objectFit="cover" />
+            <Image src="/images/Captura1.webp" alt="Playas" layout="fill" objectFit="cover" />
           </div>
+          <Gallery section="flotantes" />
+          <Testimonios section="flotantes" />
         </div>
       </div>
       <Footer />
