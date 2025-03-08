00e7adf355434f580f02cacc931b923c0c244543
sergio <sergiolopezantequera@gmail.com>
2025-03-08 14:16:04 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "tu_proyecto.firebaseapp.com",
-  projectId: "tu_proyecto",
-  storageBucket: "tu_proyecto.appspot.com",
+  authDomain: "TU_DOMAIN.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_BUCKET.appspot.com",
   messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
+  appId: "TU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
