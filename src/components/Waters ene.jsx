00468e0062ee621344d4c61b32226c693de386b9
sergio <sergiolopezantequera@gmail.com>
2025-03-09 09:01:38 +0100

    Create Waters ene.jsx

---
+++ src/components/Waters ene.jsx
@@ -0,0 +1,24 @@
+import { Canvas } from '@react-three/fiber';
+import { Water } from 'drei';
+
+export default function WaterScene() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full z-0">
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <Water
+          args={[
+            [100, 100],
+            {
+              textureWidth: 512,
+              textureHeight: 512,
+              waterColor: 0x0066cc,
+              distortionScale: 3.7,
+            }
+          ]}
+          rotation={[-Math.PI / 2, 0, 0]}
+        />
+      </Canvas>
+    </div>
+  );
+}
