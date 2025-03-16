6b5eae1e14a5d7bea5c755666cd4105c6a5dc190
sergio <sergiolopezantequera@gmail.com>
2025-03-16 03:42:09 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,12 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import HeroSection from '@/components/HeroSection';
-import Testimonios from '@/components/Testimonios';
-import ScrollAnimation from '@/components/ScrollAnimation';
 
 export default function Home() {
   return (
     <div className="relative">
       <Navbar />
       <HeroSection />
-      <ScrollAnimation>
-        <Testimonios />
-      </ScrollAnimation>
       <Footer />
     </div>
   );
