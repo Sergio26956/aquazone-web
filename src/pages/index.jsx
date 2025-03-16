da10429dba7dd6e64a7083c039715264631050f9
sergio <sergiolopezantequera@gmail.com>
2025-03-16 18:14:26 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,13 @@
 import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 import HeroSection from '@/components/HeroSection';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className="relative">
+    <>
       <Navbar />
       <HeroSection />
       <Footer />
-    </div>
+    </>
   );
 }
