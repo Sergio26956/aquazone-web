5ad89b3892f44357c35a61dd1a0876863bafcbcd
sergio <sergiolopezantequera@gmail.com>
2025-03-16 15:46:05 +0100

    Update *app.jsx

--- src/pages/*app.jsx
+++ src/pages/*app.jsx
@@ -1,13 +1,11 @@
 import '@/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-import HeroSection from '@/components/HeroSection';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <HeroSection />
       <Component {...pageProps} />
       <Footer />
     </>
