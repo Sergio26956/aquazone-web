b2cb06c2e3d7f654579eda6f0cada9d6699fa69e
sergio <sergiolopezantequera@gmail.com>
2025-03-16 15:51:55 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com'],
+    formats: ['image/webp'],
+    minimumCacheTTL: 86400
+  },
+  experimental: {
+    scrollRestoration: true
+  }
+};
