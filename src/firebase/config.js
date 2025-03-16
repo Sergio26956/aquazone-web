1d15bc16aa6cceae2cef9d74009330bc74a4e1b5
sergio <sergiolopezantequera@gmail.com>
2025-03-16 18:15:57 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyBEuXgcsioqguloc2X6QR4NCPsc9sUIWKU",
+  authDomain: "aquazoneweb-96100.firebaseapp.com",
+  projectId: "aquazoneweb-96100",
+  storageBucket: "aquazoneweb-96100.firebasestorage.app",
+  messagingSenderId: "601142060945",
+  appId: "1:601142060945:web:495745a3a90bb687805c30"
 };
 
 const app = initializeApp(firebaseConfig);
