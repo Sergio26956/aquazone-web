2eda475c60cb5fbd7a3e5de84bfa9ebb452c45c6
sergio <sergiolopezantequera@gmail.com>
2025-03-16 17:27:46 +0100

    Update index.jsx

--- src/pages/administrador/index.jsx
+++ src/pages/administrador/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/src/firebase/config';
+import { auth } from '../../firebase/config'; // ✅ Ruta corregida
 
 export default function AdminLogin() {
   const [email, setEmail] = useState('');
