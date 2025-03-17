732f7af7e21f2de0536462bc82728e355175a0c7
sergio <sergiolopezantequera@gmail.com>
2025-03-17 05:59:11 +0100

    Update kamikaze-jump.jsx

--- src/pages/parques-terrestres/kamikaze-jump.jsx
+++ src/pages/parques-terrestres/kamikaze-jump.jsx
@@ -1,11 +1,3 @@
-// ...código existente...
-<div className="relative h-96 w-full mb-12 rounded-xl overflow-hidden shadow-2xl">
-  <iframe 
-    width="100%" 
-    height="100%" 
-    src="https://www.youtube.com/embed/0ELhclay1m4" 
-    title="Kamikaze jump" 
-    allowFullScreen
-  ></iframe>
-</div>
-// ...código existente...
+export default function KamikazeJump() {
+  return <div>Kamikaze Jump</div>;
+}
