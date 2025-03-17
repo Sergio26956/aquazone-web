73f616af0d9659d7ae642e0bbdd8c439f140dc1d
sergio <sergiolopezantequera@gmail.com>
2025-03-17 05:57:45 +0100

    Update piscinas.jsx

--- src/pages/parques-flotantes/piscinas.jsx
+++ src/pages/parques-flotantes/piscinas.jsx
@@ -1,11 +1,3 @@
-// ...código existente...
-<div className="relative h-96 w-full mb-12 rounded-xl overflow-hidden shadow-2xl">
-  <iframe 
-    width="100%" 
-    height="100%" 
-    src="https://www.youtube.com/embed/rUNkup9IDe8" 
-    title="Flotante piscina" 
-    allowFullScreen
-  ></iframe>
-</div>
-// ...código existente...
+export default function Piscinas() {
+  return <div>Piscinas</div>;
+}
